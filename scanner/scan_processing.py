0dc51736019b09bcaacb3d66f982e3d4878701e6
Nauval Zulfikar <70472247+nauvalZulfikar@users.noreply.github.com>
2026-02-12 18:28:41 +0700

    Update scan_processing.py

--- scanner/scan_processing.py
+++ scanner/scan_processing.py
@@ -1,6 +1,6 @@
+# scan_processing.py
+
 from common import run_scanner
 
-DB_PATH = "./scans.db"
-
 if __name__ == "__main__":
-    run_scanner(DB_PATH, mode="Processing")
+    run_scanner(mode="Processing")
