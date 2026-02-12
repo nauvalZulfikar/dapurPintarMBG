35d60a22401bdd6e2f4917e7dbd86bea1f8eab7b
Nauval Zulfikar <70472247+nauvalZulfikar@users.noreply.github.com>
2026-02-12 18:28:28 +0700

    Update scan_packing.py

--- scanner/scan_packing.py
+++ scanner/scan_packing.py
@@ -1,6 +1,6 @@
+# scan_packing.py
+
 from common import run_scanner
 
-DB_PATH = "./scans.db"
-
 if __name__ == "__main__":
-    run_scanner(DB_PATH, mode="Packing")
+    run_scanner(mode="Packing")
